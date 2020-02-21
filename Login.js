fea8cc9836b3ba76d642b816eb658be889ca7979
Catherine <catherinejchen@hotmail.com>
2020-02-20 21:20:20 -0500

    updated login info

--- Login.js
+++ Login.js
@@ -2,8 +2,8 @@
 
 function NextPage(){
     event.preventDefault();
-    if(document.getElementById("username").value == "cathchen" &&
-    document.getElementById("password").value == "coolestperson"){
+    if(document.getElementById("username").value == "htn" &&
+    document.getElementById("password").value == "hackthenorth"){
         window.location.href = './main.html';
     }
     else{
